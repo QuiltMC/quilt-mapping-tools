0b20547c998f640a35b26e70934659013df0fd37
ByMartrixx <bymartrixx@gmail.com>
2022-08-28 01:38:37 -0400

    Fix wrong icon path

--- intellij-plugin/src/main/java/org/quiltmc/mapping/intellij/QuiltMappingIcons.java
+++ intellij-plugin/src/main/java/org/quiltmc/mapping/intellij/QuiltMappingIcons.java
@@ -21,5 +21,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.Icon;
 
 public class QuiltMappingIcons {
-	public static final Icon FILE = IconLoader.getIcon("/icons/quiltMiniIcon.svg", QuiltMappingIcons.class);
+	public static final Icon FILE = IconLoader.getIcon("/icons/quiltMiniLogo.svg", QuiltMappingIcons.class);
 }
