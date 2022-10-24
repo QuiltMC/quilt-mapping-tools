a4671aa2bb09e2a8cd542acc07b1aa17e8c80305
ByMartrixx <bymartrixx@gmail.com>
2022-10-23 22:47:12 -0300

    Add a folding builder

---
+++ intellij-plugin/src/main/java/org/quiltmc/mapping/intellij/language/QuiltMappingFoldingBuilder.java
@@ -0,0 +1,78 @@
+package org.quiltmc.mapping.intellij.language;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.folding.FoldingBuilderEx;
+import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.util.Couple;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.quiltmc.mapping.intellij.language.psi.QuiltMappingTypes;
+import org.quiltmc.mapping.intellij.language.psi.impl.QuiltMappingPsiUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuiltMappingFoldingBuilder extends FoldingBuilderEx implements DumbAware {
+	@Override
+	public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
+		List<FoldingDescriptor> descriptors = new ArrayList<>();
+		collectDescriptors(root.getNode(), document, descriptors);
+		return descriptors.toArray(FoldingDescriptor.EMPTY);
+	}
+
+	private static void collectDescriptors(ASTNode node, Document document, List<FoldingDescriptor> descriptors) {
+		IElementType type = node.getElementType();
+
+		if (type == QuiltMappingTypes.COMMENT) {
+			Couple<PsiElement> expanded = QuiltMappingPsiUtil.expandElement(node.getPsi());
+			int start = expanded.getFirst().getTextRange().getStartOffset();
+			int end = expanded.getSecond().getTextRange().getEndOffset();
+
+			if (document.getLineNumber(start) != document.getLineNumber(end)) {
+				descriptors.add(new FoldingDescriptor(node, new TextRange(start, end)));
+			}
+		} else if (type == QuiltMappingTypes.BLOCK_COMMENT) {
+			descriptors.add(new FoldingDescriptor(node, node.getTextRange()));
+		} else if (type == QuiltMappingTypes.OBJECT || type == QuiltMappingTypes.ARRAY || type == QuiltMappingTypes.DOUBLE_QUOTED_STRING || type == QuiltMappingTypes.SINGLE_QUOTED_STRING) {
+			TextRange range = node.getTextRange();
+			if (document.getLineNumber(range.getStartOffset()) < document.getLineNumber(range.getEndOffset())) {
+				descriptors.add(new FoldingDescriptor(node, range));
+			}
+		}
+
+		for (ASTNode child : node.getChildren(null)) {
+			collectDescriptors(child, document, descriptors);
+		}
+	}
+
+	@Override
+	public @Nullable String getPlaceholderText(@NotNull ASTNode node) {
+		IElementType type = node.getElementType();
+
+		if (type == QuiltMappingTypes.BLOCK_COMMENT) {
+			return "/*...*/";
+		} else if (type == QuiltMappingTypes.COMMENT) {
+			return "//...";
+		} else if (type == QuiltMappingTypes.OBJECT) {
+			return "{...}";
+		} else if (type == QuiltMappingTypes.ARRAY) {
+			return "[...]";
+		} else if (type == QuiltMappingTypes.DOUBLE_QUOTED_STRING) {
+			return "\"...\"";
+		} else if (type == QuiltMappingTypes.SINGLE_QUOTED_STRING) {
+			return "'...'";
+		}
+
+		return "...";
+	}
+
+	@Override
+	public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+		return false;
+	}
+}
---
+++ intellij-plugin/src/main/java/org/quiltmc/mapping/intellij/language/psi/impl/QuiltMappingPsiUtil.java
@@ -0,0 +1,38 @@
+package org.quiltmc.mapping.intellij.language.psi.impl;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.Couple;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.quiltmc.mapping.intellij.language.psi.QuiltMappingTypes;
+
+import java.util.List;
+
+public class QuiltMappingPsiUtil {
+	private static final List<IElementType> COMMENT_TYPES = List.of(QuiltMappingTypes.COMMENT, QuiltMappingTypes.BLOCK_COMMENT);
+
+	public static PsiElement getLastSiblingOfType(@NotNull PsiElement start, IElementType expectedType, boolean forwards) {
+		ASTNode node = start.getNode();
+		ASTNode current = node;
+
+		while (node != null) {
+			IElementType elementType = node.getElementType();
+			if (elementType == expectedType) {
+				current = node;
+			} else if (elementType != TokenType.WHITE_SPACE && (!COMMENT_TYPES.contains(elementType) || COMMENT_TYPES.contains(expectedType))) {
+				break;
+			}
+
+			node = forwards ? node.getTreeNext() : node.getTreePrev();
+		}
+
+		return current.getPsi();
+	}
+
+	public static Couple<PsiElement> expandElement(PsiElement element) {
+		IElementType type = element.getNode().getElementType();
+		return Couple.of(getLastSiblingOfType(element, type, false), getLastSiblingOfType(element, type, true));
+	}
+}
